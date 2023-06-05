62a7e1772276bfbdfa9b23b1b5a457823464b22f
EduardoR99 <dudurodrigues99@hotmail.com>
2023-06-05 12:36:11 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar_container">
+        <div class="icon_navbar">
+
+        </div>
+        <div class="text_navbar">
+            <p>Company <div class="text_colored">Register</div></p>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+
+</style>
